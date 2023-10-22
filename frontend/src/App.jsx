630f7f25e52458af11507820347736b9b7566474
Alexandre Gressier <alexandre@gressier.dev>
2023-10-23 00:28:05 +0200

    Fetch a number according to current URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,28 @@ const apiUrl = "http://localhost:3000/api"
 
 function App() {
   const [fact, setFact] = useState("")
+  let number = window.location.pathname.split("/").pop()
 
   useEffect(() => {
     async function fetchFact() {
-      try {
-        const response = await fetch(`${apiUrl}/facts/42`)
-        if (!response.ok) {
-          throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+      if (number) {
+        try {
+          const response = await fetch(`${apiUrl}/facts/${number}}`)
+          if (!response.ok) {
+            throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+          }
+          const result = await response.json()
+          setFact(result.fact)
+        } catch (error) {
+          console.error("Error while fetching number fact from API.", error)
+          setFact("Error! Please try again.")
         }
-        const result = await response.json()
-        setFact(result.fact)
-      } catch (error) {
-        console.error("Error while fetching number fact from API.", error)
-        setFact("Error! Please try again.")
+      } else {
+        setFact("Please provide a number in the URL.")
       }
     }
     fetchFact()
-  }, [])
+  }, [number])
 
   return <>
     <h1>

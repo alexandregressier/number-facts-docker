3c0c9ea016131179697859c3dff2cf28b6883f17
Alexandre Gressier <alexandre@gressier.dev>
2023-10-23 00:48:12 +0200

    Fix number parsing in the URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,28 +5,33 @@ const apiUrl = "http://localhost:3000/api"
 
 function App() {
   const [fact, setFact] = useState("")
-  let number = window.location.pathname.split("/").pop()
+  let numberString = window.location.pathname.split("/").pop()
 
   useEffect(() => {
     async function fetchFact() {
-      if (number) {
-        try {
-          const response = await fetch(`${apiUrl}/facts/${number}}`)
-          if (!response.ok) {
-            throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
-          }
-          const result = await response.json()
-          setFact(result.fact)
-        } catch (error) {
-          console.error("Error while fetching number fact from API.", error)
-          setFact("Error! Please try again.")
-        }
-      } else {
+      if (!numberString) {
         setFact("Please provide a number in the URL.")
+        return
+      }
+      const number = Number(numberString)
+      if (isNaN(number)) {
+        setFact("This is not a number.")
+        return
+      }
+      try {
+        const response = await fetch(`${apiUrl}/facts/${number}`)
+        if (!response.ok) {
+          throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+        }
+        const result = await response.json()
+        setFact(result.fact)
+      } catch (error) {
+        console.error("Error while fetching number fact from API.", error)
+        setFact("Error! Please try again.")
       }
     }
     fetchFact()
-  }, [number])
+  }, [numberString])
 
   return <>
     <h1>

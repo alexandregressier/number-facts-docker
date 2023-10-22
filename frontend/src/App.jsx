5cc31fb373d5614303c747cd1f04e8d01777930a
Alexandre Gressier <alexandre@gressier.dev>
2023-10-23 00:18:08 +0200

    Make the frontend request the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css"
+import {useEffect, useState} from "react"
+
+const apiUrl = "http://localhost:3000/api"
 
 function App() {
+  const [fact, setFact] = useState("")
+
+  useEffect(() => {
+    async function fetchFact() {
+      try {
+        const response = await fetch(`${apiUrl}/facts/42`)
+        if (!response.ok) {
+          throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+        }
+        const result = await response.json()
+        setFact(result.fact)
+      } catch (error) {
+        console.error("Error while fetching number fact from API.", error)
+        setFact("Error! Please try again.")
+      }
+    }
+    fetchFact()
+  }, [])
 
   return <>
     <h1>
-      Hello, world!
+      {fact ? fact : "Loading…"}
     </h1>
   </>
 }

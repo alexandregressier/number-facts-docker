312b328b7a7679f37d61a8f35d1257279ebe6cee
Alexandre Gressier <alexandre@gressier.dev>
2023-10-22 23:37:58 +0200

    Make the backend use Redis

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,40 @@
 const express = require("express")
 const index = express()
 const router = express.Router()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const redis = require("redis")
+const redisHost = process.env.REDIS_HOST || "localhost"
+const redisPort = process.env.REDIS_PORT || 6379
+const client = redis.createClient({url: `redis://${redisHost}:${redisPort}`})
 
 router.get("/facts/:number", async (req, res) => {
   const number = req.params.number
 
   try {
-    const response = await fetch(`http://numbersapi.com/${number}`)
-    if (!response.ok) {
-      throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+    const result = await client.get(number)
+    if (result) {
+      console.log(`Retrieved number fact for ${number} from Redis`)
+      res.json({
+        number: number,
+        fact: result,
+      })
+    } else {
+      const response = await fetch(`http://numbersapi.com/${number}`)
+      if (!response.ok) {
+        throw new Error(`HTTP Error ${response.status} while retrieving number fact for ${number}`)
+      }
+      const result = await response.text()
+      await client.set(number, result)
+      console.log(`Retrieved number fact for ${number} from API.`)
+      res.json({
+        number: number,
+        fact: result,
+      })
     }
-    const data = await response.text()
-    res.json({
-      number: number,
-      fact: data,
-    })
   } catch (error) {
     res.json({
-      message: `Error retrieving number fact for ${number}`,
+      message: `Error retrieving number fact for ${number}.`,
       error: error.toString(),
     })
   }
@@ -26,6 +42,9 @@ router.get("/facts/:number", async (req, res) => {
 
 index.use("/api", router)
 
-index.listen(port, () => {
+index.listen(port, async () => {
+  console.log(`Connecting to Redis at redis://${redisHost}:${redisPort}…`)
+  await client.connect();
+  console.log("Connected to Redis.")
   console.log(`Backend started at http://localhost:${port}`)
 })
